c13106de8e2d82fb82344b608908f48f979aed7a
Julian Nadarzy <julian.nadarzy@gmail.com>
2022-01-03 16:12:09 +0100

    Initial commit

---
+++ src/main/java/com/nadarzy/inboxapp/InboxAppApplication.java
@@ -0,0 +1,24 @@
+package com.nadarzy.inboxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class InboxAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InboxAppApplication.class, args);
+    }
+
+    @RequestMapping("/user")
+    public String user(@AuthenticationPrincipal OAuth2User principal) {
+        System.out.println(principal);
+        System.out.println("hi");
+        return principal.getAttribute("name");
+    }
+}
